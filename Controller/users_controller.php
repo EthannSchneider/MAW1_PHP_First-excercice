d8b3b987723d4d4cfa3d21a0a2aab90c06a84dfd
Ethann Schneider <pf70xyr@eduvaud.ch>
2024-08-22 09:44:42 +0200

    Updated for windows use

--- Controller/users_controller.php
+++ Controller/users_controller.php
@@ -3,28 +3,22 @@ function add_user() {
     if(isset($_POST['name'])) {
         $users_instance = new Users();
         $users_instance->add_user($_POST['name']);
-
-        header('Location: /');
     }
-    lost();
+    header('Location: /');
 }
 
 function delete_user() {
     if(isset($_POST['name'])) {
         $users_instance = new Users();
         $users_instance->delete_user($_POST['name']);
-
-        header('Location: /');
     }
-    lost();
+    header('Location: /');
 }
 
 function edit_user() {
     if(isset($_POST['name']) && isset($_POST['html_level']) && isset($_POST['php_base_level']) && isset($_POST['php_oo_level']) && isset($_POST['sql_level'])) {
         $users_instance = new Users();
         $users_instance->edit_user($_POST['name'], $_POST['html_level'], $_POST['php_base_level'], $_POST['php_oo_level'], $_POST['sql_level']);
-
-        header('Location: /');
     }
-    lost();
+    header('Location: /');
 }
